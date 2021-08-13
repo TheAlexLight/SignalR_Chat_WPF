96e8a172340e0362508eeb3753fd8f299384af1e
Oleksandr Yudin <oyudi@softserveinc.com>
2021-08-13 14:05:13 +0300

    PasswordBox validation hints were added

---
+++ ChatClient/Validators/EmailValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ChatClient.Validators
+{
+    class EmailValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            ValidationResult result = new ValidationResult(true, null);
+
+            try
+            {
+                new MailAddress(value.ToString());
+            }
+            catch
+            {
+                result = new ValidationResult(false, "Email is not valid");
+            }
+
+            return result;
+        }
+    }
+}
