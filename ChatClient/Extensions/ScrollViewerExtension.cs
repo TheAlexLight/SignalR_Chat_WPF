2a78ef19f565f8a73527c8fc6ef85a56c6b1102d
TheAlexLight <sasha15452@gmail.com>
2021-10-24 13:23:35 +0300

    Unused scroll data were removed

--- ChatClient/Extensions/ScrollViewerExtension.cs
+++ ChatClient/Extensions/ScrollViewerExtension.cs
@@ -14,38 +14,12 @@ namespace ChatClient.Extensions
 {
     public class ScrollViewerExtension : ScrollViewer
     {
-        //// Create a custom routed event by first registering a RoutedEventID
-        //// This event uses the bubbling routing strategy
-        //public static readonly RoutedEvent CustomScrollChangedEvent = EventManager.RegisterRoutedEvent(
-        //    nameof(CustomScrollChanged), RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(ScrollViewerExtension));
-
-        //// Provide CLR accessors for the event
-        //public event RoutedEventHandler CustomScrollChanged
-        //{
-        //    add { AddHandler(CustomScrollChangedEvent, value); }
-        //    remove { RemoveHandler(CustomScrollChangedEvent, value); }
-        //}
-
-        //// This method raises the Tap event
-        //void RaiseCustomScrollChangedEvent()
-        //{
-        //    RoutedEventArgs newEventArgs = new RoutedEventArgs(CustomScrollChangedEvent);
-        //    RaiseEvent(newEventArgs);
-        //}
-
         private bool _messagesHandled;
-        private double _scrollOffset;
-        private ScrollViewerExtension _scrollViewerExtension;
 
-        public static readonly DependencyProperty AlwaysScrollToEndProperty;
         public static readonly DependencyProperty MessageCollectionProperty;
-        public static readonly DependencyProperty MessageHeightProperty;
         
         static ScrollViewerExtension()
         {
-            AlwaysScrollToEndProperty = DependencyProperty.RegisterAttached("AlwaysScrollToEnd"
-              , typeof(bool), typeof(ScrollViewerExtension), new PropertyMetadata(false, AlwaysScrollToEndChanged));
-
             MessageCollectionProperty = DependencyProperty.Register(nameof(MessageCollection)
             , typeof(List<MessageModel>), typeof(ScrollViewerExtension), new PropertyMetadata(new List<MessageModel>(), MessagesChanged));
         }
@@ -72,26 +46,6 @@ namespace ChatClient.Extensions
         {
             get => (List<MessageModel>)GetValue(MessageCollectionProperty);
             set => SetValue(MessageCollectionProperty, value);
-        }
-
-        public static bool GetAlwaysScrollToEnd(ScrollViewer scroll) => (bool)scroll.GetValue(AlwaysScrollToEndProperty);
-        public static void SetAlwaysScrollToEnd(ScrollViewer scroll, bool alwaysScrollToEnd) => scroll.SetValue(AlwaysScrollToEndProperty, alwaysScrollToEnd);
-
-        private static void AlwaysScrollToEndChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (sender is ScrollViewerExtension scroll)
-            {
-                bool alwaysScrollToEnd = (e.NewValue != null) && (bool)e.NewValue;
-
-                if (alwaysScrollToEnd)
-                {
-                    scroll._scrollViewerExtension = scroll;
-                }
-            }
-            else
-            {
-                throw new InvalidOperationException("The attached AlwaysScrollToEnd property can only be applied to ScrollViewer instances.");
-            }
         }
     }
 }
